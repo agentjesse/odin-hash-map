cb51b1935a18789dd242c9a1535230a9c59f6ee3
jesse <31576321+agentjesse@users.noreply.github.com>
2024-04-04 22:50:13 -0400

    hash function changed.
    fancy internet ones not needed, just using a prime number larger than alphabet
    and capacity for modulo operation. will implement bucket array growth later.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /* Next task:
-- when you get to the point of storing key value pairs in buckets, keep a set or array with them for methods like has()=boolean
+- when you get to the point of storing key-value pairs in buckets, keep a set or array with them for methods like has()=boolean
 
--apply modulo on each iteration of loop in hashing fn to avoid large numbers in javascript. pass in capacity of hashmap instead of leaving null default.
+-linked lists are too large, need to implement array growth via 0.75 load factor and bucket fullness.
 
 */
 
@@ -10,26 +10,38 @@
 import { logToConsole as lg, tableToConsole as tb } from './logger.js'; //shorthand loggers
 import makeLinkedList from './linkedList.js'; //default import example
 
-//string hashing fn
-const getHashCode = (key, capacity = null)=> {
+//string hashing fn returns hashcode which is also bucket address. Passing in bucket
+//capacity here to use the modulo operator in each loop; this keeps the numbers small
+//for JS accuracy and also prevents them ever being more than capacity.
+const getHashCode = (key, capacity)=> {
   let hashCode = 0;
-  //todo: implement modulo using capacity
   for ( let i = 0; i < key.length; i++ ) {
-    //31 is odd prime number close to size of alphabet
-    hashCode = 31 * hashCode + key.charCodeAt(i);
+    //31 is odd prime number related to alphabet size
+    hashCode = (31 * hashCode + key.charCodeAt(i)) % capacity;
   }
+  lg( `hashCode: ${ hashCode }` );
   return hashCode;
 };
 
 //hashmap creation factory fn, yay no overthinking 'this'.
 const makeHashMap = ()=> {
+  //start with default size of 16 buckets. todo: implement array growth
   let buckets = new Array(16); //or use Array.from() for mapping fn if needed
-  // lg( buckets.length );
+  const loadFactor = 0.75;
 
-  //fn to set key value pair in bucket
+  //fn to set key-value pair in bucket
   const set = (key, value)=> {
-    const storingIndex = getHashCode(key) % 16; //todo: apply modulo during hashing
-    buckets[storingIndex] = value;
+    const bucketIndex = getHashCode(key, buckets.length);
+    // lg( `bucketIndex: ${ bucketIndex }` );
+
+    //create or append linked list
+    if ( buckets[bucketIndex] === undefined ) { //when bucket empty
+      const newLinkedList = makeLinkedList(); //make linked list
+      newLinkedList.append( [key, value] ); //add key-value pair value node
+      buckets[bucketIndex] = newLinkedList;
+      // lg( `stored key-value pair in bucket: ${ bucketIndex }` );
+    }
+
   };
 
   return {
@@ -38,7 +50,7 @@ const makeHashMap = ()=> {
 };
 
 //-----------testing
-//customer/item key/value pairs for testing:
+//customer-item key-value pairs for testing:
 const namesAndCartItemsArr = [
   ['John Smith', 'Toilet Paper'],
   ['Emily Johnson', 'Bottled Water'],
@@ -59,11 +71,26 @@ const namesAndCartItemsArr = [
   ['Matthew Thompson', 'Frozen Pizza'],
   ['Amelia Garcia', 'Paper Towels'],
   ['Samuel Robinson', 'Orange Juice'],
-  ['Ella Lee', 'Salmon']
+  ['Ella Lee', 'Salmon'],
+  ['Benjamin Clark', 'Yogurt'],
+  ['Lily Rodriguez', 'Avocado'],
+  ['Christopher Lewis', 'Potatoes'],
+  ['Grace Walker', 'Cucumber'],
+  ['Andrew Hall', 'Onions'],
+  ['Madison Allen', 'Lettuce'],
+  ['Lucas Young', 'Peanut Butter'],
+  ['Avery Wright', 'Ice Cream'],
+  ['Evelyn King', 'Broccoli'],
+  ['Jackson Scott', 'Ground Turkey']
 ];
-lg( `'John Smith' hash: ${  getHashCode( namesAndCartItemsArr[0][0] )}` ); //hash fn test from string key
+// lg( `'John Smith' hash: ${  getHashCode( namesAndCartItemsArr[0][0] )}` ); //hash fn test from string key
 // lg( getHashCode( 'JohnSmith' ) ); //hash fn test: no space
 // lg( getHashCode( 'SmithJohn' ) ); //hash fn test: permutation
 const namesAndCartItemsMap = makeHashMap();
-namesAndCartItemsMap.set( namesAndCartItemsArr[0][0], namesAndCartItemsArr[0][1] );
+//set single entry in HashMap
+// namesAndCartItemsMap.set( namesAndCartItemsArr[0][0], namesAndCartItemsArr[0][1] );
+//set full data in Hash Map
+namesAndCartItemsArr.forEach( ([key, value])=> {
+  namesAndCartItemsMap.set( key, value );
+} );
 lg( namesAndCartItemsMap );
